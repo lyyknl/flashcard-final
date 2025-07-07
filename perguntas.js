cff96f584f95a9d4c030dbef878dfdab76ca1058
Analy Knaul <analy.knaul@escola.pr.gov.br>
2025-07-07 16:26:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Astronomia',
+    'Por que Plutão deixou de ser considerado um planeta?',
+    'Pois não limpa a sua órbita de outros objetos.'
+)
+criaCartao(
+    'Biologia',
+    'Qual é a molecula responsável pelo transporte de oxigênio no sangue?',
+    'Hemoglobina'
+)
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico do Ferro?',
+    'Fe'
+)
+criaCartao(
+    'Filosofia',
+    'Quem disse a frase "Penso, logo existo"?' ,
+    'René Descartes'
+)
+criaCartao(
+    'filosofia',
+    ' Qual corrente filosófica defende que o conhecimento vem da experiência sensorial?',
+    ' Empirismo'
+)
+criaCartao(
+    'Química',
+    ' O que caracteriza uma reação endotérmica?',
+    'Absorção de calor'
+)
+criaCartao(
+    'Geografia',
+    'Qual processo geológico é responsável pela formação de montanhas como os Andes e o Himalaia?',
+    'Movimento das placas tectônicas.'
+)
+criaCartao(
+    'Biologia',
+    'Qual organela celular é responsável pela produção de energia?',
+    'Mitocôndria'
+
+)
